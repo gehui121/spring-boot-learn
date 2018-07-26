d1f213c532671757fb54022bf791bd5a42af8e47
gehui121 <gehui121@163.com>
2018-07-26 15:48:21 +0800

    SpringBoot整合JavaMail发送邮件

---
+++ demo_07/src/main/java/com/gehui/Demo07Application.java
@@ -0,0 +1,12 @@
+package com.gehui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo07Application.class, args);
+    }
+}
