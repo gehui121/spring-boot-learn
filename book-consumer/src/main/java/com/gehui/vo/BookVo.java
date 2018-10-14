f4abc1d407759776acbbcbe52d5911ba808ee3e1
gehui121 <gehui121@163.com>
2018-10-14 18:03:59 +0800

    SpringBoot开发查询图书信息消费者

---
+++ book-consumer/src/main/java/com/gehui/vo/BookVo.java
@@ -0,0 +1,55 @@
+package com.gehui.vo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2018/10/13 15:55.
+ **/
+public class BookVo implements Serializable {
+    private Long bookId;
+    private String bookAuthor;
+    private String bookName;
+    private double bookPrice;
+    private Date bookDate;
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public double getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(double bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    public Date getBookDate() {
+        return bookDate;
+    }
+
+    public void setBookDate(Date bookDate) {
+        this.bookDate = bookDate;
+    }
+}
